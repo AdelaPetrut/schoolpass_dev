ae29bd13e160e55e9bd851b3c11739591a61caec
Tut Andrei <andrei.tut@synergogroup.net>
2018-06-05 16:38:00 +0300

    added internal classes.

---
+++ Solutrack.Notifications.Api/Solutrack.Notifications.PushNotifications/Contracts/Entities/IResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutrack.Notifications.PushNotifications.Contracts.Entities
+{
+    public interface IResponse
+    {
+        string Message { get; set; }
+        bool IsSuccess { get; set; }
+    }
+
+    public interface IResponse<T> : IResponse
+    {
+        T Value { get; set; }
+    }
+}
---
+++ Solutrack.Notifications.Api/Solutrack.Notifications.PushNotifications/Implementation/Entities/Enums.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solutrack.Notifications.PushNotifications.Implementation.Entities
+{
+    public class Enums
+    {
+        public enum Platform
+        {
+            apns,
+            gcm
+        }
+    }
+}
